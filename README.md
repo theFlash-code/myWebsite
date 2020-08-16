19c42c710579522499d6115baa067e67c224fc9b
theFlash-code <alan.tao.no1@gmail.com>
2020-08-16 22:27:51 +0800

    add another two cards

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# just get started
+# My Website
+Trying to create a website for myself to actually practice my skills on web developing
--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-let open = false;
 
-let statusCard = function(){
-    let pic = document.querySelector(".card-img");
-    let text_box = document.querySelector(".card-text");
-    let text = document.querySelector(".card-text p");
+let picArray = document.querySelectorAll(".card-div");
+let picNum = picArray.length;
+let open = new Array(picNum);
+for(let i=0; i<picNum; ++i){
+    open[i] = false;
+}
 
-    if(!open){
+let statusCard = function(id){
+
+    let pic = picArray[id].firstElementChild;
+    let text_box = pic.nextElementSibling;
+    let text = text_box.firstElementChild;
+
+    if(open[id] == false){
         pic.style.transitionDelay = "0s";
         pic.style.borderBottomLeftRadius = "0";
         pic.style.borderBottomRightRadius = "0";
         text_box.style.height = "350px";
         text.style.opacity = "1";
-        open = true;
+        open[id] = true;
     }else{
         pic.style.transitionDelay = "0.8s";
         pic.style.borderBottomLeftRadius = "10px";
         pic.style.borderBottomRightRadius = "10px";
         text_box.style.height = "0";
         text.style.opacity = "0";
-        open = false;
+        open[id] = false;
     }
+    setTimeout(() => { pic.style.transitionDelay = "0s";}, 800);
 }
